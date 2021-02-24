dfcedbfbc833b55705e5f4a78adbee44bf67462b
Douglas Shibata <douglas.shibata11@gmail.com>
2021-02-24 10:03:59 -0300

    Mudando a url do video para produçao

--- src/pages/VideoChamada/index.js
+++ src/pages/VideoChamada/index.js
@@ -8,14 +8,16 @@ export default function VideoChamada(props) {
     function Iframe(props) {
         return (<div dangerouslySetInnerHTML={{ __html: props.iframe ? props.iframe : "" }} />);
     }
-    const iframe1 = `
+    //Local
+    const iframe2 = `
     <iframe
       src="https://tokbox.com/embed/embed/ot-embed.js?embedId=3f921956-053c-4671-aa28-dc2a4296cd44&room=DEFAULT_ROOM&iframe=true"
       scrolling="auto"
       allow="microphone; camera"
     ></iframe>
     `; 
-    const iframe2 = `
+    //Produção
+    const iframe1 = `
     <iframe
     src="https://tokbox.com/embed/embed/ot-embed.js?embedId=1c9ba970-9e31-423a-a7ad-27c15d4ab4b3&room=DEFAULT_ROOM&iframe=true"
     scrolling="auto"
